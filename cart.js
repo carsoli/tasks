8570431010505d51cc5f3a139c265109df95338f
carsoli <caroleemad96@gmail.com>
2017-04-13 00:28:38 +0200

    done w/ testing

--- cart.js
+++ cart.js
@@ -1,109 +1,130 @@
-module.exports.product = function(id, name, price){
-    this.id = id;
-    this.name = name;
-    this.price = price;
+var Product ={
+    create: function(id, name, price){
+        var instance = Object.create(this); //Object.create(proto, [properties](optional));
+        instance.id = id; 
+        instance.name = name; 
+        instance.price = price;
+        return instance; 
+    }    
 }
 
 
-//private
-module.exports.item = function(product, opts){
-    this.product =  new product(product.id, product.name, product.price);  
-    if(opts['count']){// method overloading
-        this.count = opts['count']; 
-        this.price = (product.price)*(opts['count']);
-    }else{
-        this.count = 1;
-        this.price = (product.price)*1; //compound price of this item
+var Item = {
+    create: function(product, opts){
+        var instance = Object.create(this); 
+        instance.product = product;//Product.create(product.id, product.name, product.price);
+        if(opts['count']){// method overloading
+            instance.count = opts['count']; 
+            instance.price = (product.price)*(opts['count']);
+        }else{
+            instance.count = 1;
+            instance.price = (product.price)*1; //compound price of this item
+        }
+        return instance;
+    } 
+}
+
+
+var Cart =  {
+    create: function(){
+        var instance = Object.create(this);
+        instance.items = [];
+        return instance; 
+    },
+
+
+    getItems: function(){
+        return this.items;
+    },
+
+
+    hasProduct: function(product){
+        var flag = false;
+        this.items.forEach(function(item){
+            // console.log("item id: " + item.product.id);
+            // console.log("input id: " +product.id);
+            if(item.product.id == product.id)
+                flag = true;
+        }); 
+        return flag;    
+    },
+
+
+    addItem: function(product){
+        // console.log(this);
+        var exists= false;
+        this.items.forEach(function(item){
+            if((item.product.id == product.id) &&(exists==false)){
+                item.count++; 
+                item.price += product.price;
+                exists = true; 
+            }
+        });
+        if(exists ==false){
+            var newItem = Item.create(product, {});
+            // console.log("new item is: "+ newItem.product.id);
+            this.items.push(newItem);
+        }
+    },
+
+
+    removeItem: function(product){
+        var nremoved =0;
+        this.items.forEach((item,index)=>{
+            if(item.product.id == product.id){
+                if(item.count>0){
+                    item.count--; 
+                    item.price -= product.price;
+                }else{
+                    this.items.splice(index,1);
+                }    
+                nremoved++;
+            }
+        });
+        if(nremoved ==0)
+            console.log("Attempting to remove an item that does not exist in Cart");
+    },
+
+
+    removeProduct: function(product){
+        var nremoved=0;
+        this.items.forEach((item,index)=>{
+            if(item.product.id==product.id){
+                nremoved+= item.count;
+                console.log("Number of items removed = " + nremoved);
+                this.items.splice(index,1);
+            }
+        });
+        if(nremoved ==0)
+            console.log("Attempting to remove an item that does not exist in Cart");
+    },
+
+
+    getItem: function(product){
+        var output = null;
+        this.items.forEach(function(item){
+            if((item.product.id == product.id) && (output == null)){
+                output = item;
+            }
+        });
+        return output;
+    },
+
+
+    getTotal: function(){
+        var total= 0;
+        this.items.forEach((item)=>{
+            total+= item.price;//compoud price of each item
+        });
+        return total;
+    },
+
+
+    clearCart: function(){
+       this.items.forEach((item)=>{
+            this.items.pop();
+        });
     }
 }
 
-
-module.exports.cart = function(){
-    this.items = [];
-}
-
-
-module.exports.getItems = function(){
-    return this.items;
-}
-
-
-module.exports.hasProduct = function(product){
-    this.items.forEach((item)=>{
-        if(item.product.id == product.id)
-            return true;
-    }); 
-    return false;    
-}
-
-
-module.exports.addItem= function(product){
-    this.items.forEach((item)=>{
-        if(item.product.id == product.id){
-            item.count++; 
-            item.price += product.price;
-            return;
-        }
-    });
-    // var newProduct = new product(product.id, product.name, product.price);
-    var newItem = new item(product, {});
-    this.items.push(newItem);
-    return;
-}
-
-
-module.exports.removeItem = function(product){
-    this.items.forEach((item,index)=>{
-        if(item.product.id == product.id){
-            if(item.count>0){
-                item.count--; 
-                item.price -= product.price;
-            }else{
-                this.items.splice(index,1);
-            } 
-         return;   
-        }
-    });
-    console.log("attempting to remove an item that does not exist in cart");
-    return;
-}
-
-
-module.exports.removeProduct = function(product){
-    this.items.forEach((item,index)=>{
-        if(item.product.id==product.id){
-            this.items.splice(index,1);
-            return;
-        }
-    });
-    console.log("attempting to remove an item that does not exist in cart");
-    return;
-}
-
-
-
-module.exports.getItem = function(product){
-    this.items.forEach((item)=>{
-        if(item.product.id == product.id)
-        {
-            return item;
-        }
-    });
-    return null;
-}
-
-
-module.exports.getTotal = function(){
-    var total;
-    this.items.forEach((item)=>{
-        total+= item.price;//compoud price of each item
-    });
-    return total;
-}
-
-
-module.exports.clearCart = function(){
-    this.items.forEach((item)=>{
-        this.items.pop();
-    });
-}
+module.exports = {Product, Item, Cart};
--- cart.test.js
+++ cart.test.js
@@ -1,30 +1,34 @@
-require('./cart.js');
+var exp = require('./cart.js');
 
-var p1 = new product( 1, 'cheese', 20);
-// var p1 = new product( 2, 'milk', 10);
+var p1 = exp.Product.create(1, 'cheese', 20);
+var p2 = exp.Product.create( 2, 'milk', 10);
 
-// var p2 = {id: 2,name: 'milk', price: 10};
+var c1 = exp.Cart.create();
 
-var c1 = new cart();
+console.log(c1.getItems()); //initially empty
 
-console.log(c1.getItems());
+c1.addItem(p1); 
+console.log(c1.getItems()); //should return 1 instance of p1 in cart (*)
 
-// c1.addItem(p1);
-// console.log(c1.hasProduct(p1));
+console.log(c1.hasProduct(p1)); //should return true
+console.log(c1.hasProduct(p2)); //should return false
 
-// c1.addItem(p1);
-// console.log(c1.getItem(p1));
+c1.addItem(p1); 
+console.log(c1.getItem(p1)); //1 instance of p1 with count =2 & price = 20*2
 
-// c1.removeItem(p1);
-// console.log(c1.getItems());
+c1.removeItem(p1);  //back to (*) --no console log here
+c1.removeItem(p2); //but p2 doesn't exist in cart?? (error handled)
 
-// c1.addItem(p2);
-// console.log(c1.getItems());
+console.log(c1.getItems()); //same as (*) --logged
 
-// console.log(c1.getTotal());
+c1.addItem(p2);
+c1.addItem(p1);
+console.log(c1.getItems()); //2 instances of 2 different products (p1 w/count= 2, p2 w/ count= 1)
 
-// c1.removeProduct(p1);
-// console.log(c1.getItems());
+console.log(c1.getTotal()); //expected total = 50
 
-// c1.clearCart();
-// console.log(c1.getItems());
+c1.removeProduct(p1);
+console.log(c1.getItems()); //1 instance of p2
+
+c1.clearCart();
+console.log(c1.getItems()); //empty array 
